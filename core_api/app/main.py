b81b42428122e70cf1f095bf7740870e46850354
zcpefaisal <inboxlions@gmail.com>
2026-07-11 22:58:59 +0600

    refactor: remove deprecated Kafka consumer from core API

--- core_api/app/main.py
+++ core_api/app/main.py
@@ -11,7 +11,6 @@ from slowapi.errors import RateLimitExceeded
 from app.core.limiter import limiter
 # Importing the Kafka manager
 from app.core.kafka_producer import kafka_manager
-from app.core.kafka_consumer import consume_cv_uploads
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 logger = logging.getLogger(__name__)
@@ -22,24 +21,12 @@ async def lifespan(app: FastAPI):
     logger.info("Starting up the application...")
     await kafka_manager.start()
 
-    # Start the Kafka consumer task for CV uploads
-    logger.info("Starting Kafka consumer for CV uploads...")
-    consumer_task = asyncio.create_task(consume_cv_uploads())
     yield # Here the app will be running mode
     
     # --- Shutdown events ---
     logger.info("Shutting down the application...")
     await kafka_manager.stop()
     logger.info("Kafka producer stopped successfully.")
-    # Cancel the consumer task if it's still running
-    consumer_task.cancel()
-    logger.info("Waiting for Kafka consumer task to finish...")
-
-
-    try:
-        await consumer_task
-    except asyncio.CancelledError:
-        pass
 
 
 app = FastAPI(title="Enterprice Scalable Async Backend", version="1.0.0", lifespan=lifespan)
